a68008b796d44022c3a1b26c7e0b36fa97d21e5b
aditk10 <adityaaryanto31@guru.smk.belajar.id>
2025-11-20 10:45:03 +0700

    Update_Link_Audio
    Update Link antar menu
    Update Suara

---
+++ Assets/Script/NavigasiUtama.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NavigasiUtama : MonoBehaviour
+{
+    public AudioSource BGM_Gameplay;
+
+    public AudioSource BGM_MainMenu;
+
+    public AudioSource SFX_GameOver;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        // if (scene.name == "GameOver")
+        // {
+        //     StartCoroutine(delaySuara());           
+        // }
+    }
+
+    public void Credit()
+    {
+        BGM_MainMenu.Stop();
+        // _SFXTombolKlik.Play();
+        SceneManager.LoadScene("Credit");
+    }
+    public void MainMenu()
+    {
+        BGM_MainMenu.Stop();
+        // _SFXTombolKlik.Play();
+        SceneManager.LoadScene("MainMenu");
+        // skor.SimpanSkorTertinggi();
+    }
+    public void Exit()
+    {
+        // skor.SimpanSkorTertinggi();
+        StartCoroutine(DelayExit());
+    }
+    public void GamePlay()
+    {
+        StartCoroutine(DelayPlay());
+        // skor.SimpanSkorTertinggi();
+        SceneManager.LoadScene("GamePlay");
+       
+    }
+
+    public IEnumerator delaySuara()
+    {
+        // _SFXEndGame.Play();
+        yield return new WaitForSeconds(2f);
+        BGM_MainMenu.Play();
+    }
+
+    public IEnumerator DelayExit()
+    {
+        BGM_MainMenu.Stop();
+        // _SFXExit.Play();
+        yield return new WaitForSeconds(0.1f);
+        Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public IEnumerator DelayPlay()
+    {
+        BGM_MainMenu.Stop();
+        // _SFXStartGame.Play();
+        yield return new WaitForSeconds(1f);
+    }
+
+}
